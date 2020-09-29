b97d369da39f5dc32a7c9d04b9717f56c22ccd2e
schakravarti <schakravarti930@gmail.com>
2020-09-29 19:43:12 +0530

    [Swapnil] Add : Monthly Wage Calculation Using Class Methods

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -4,40 +4,9 @@ namespace EmployeeWageComputation
 {
     class Program
     {
-        public const int IS_FULL_TIME = 1;
-        public const int IS_PART_TIME = 2;
-        public const int EMP_RATE_PER_HOUR = 20;
-        public const int NUM_OF_WORKING_DAYS = 20;
-        public const int MAX_HRS_IN_MONTH = 100;
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation Problem");
-            int empHrs = 0;
-            int empWage = 0;
-            int totalEmpWage = 0;
-            int totalEmpHrs = 0;
-            int totalWorkingDays = 0;
-            Random random = new Random();
-            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
-            {
-                int empCheck = random.Next(0, 3);
-                totalWorkingDays++;
-                switch (empCheck)
-                {
-                    case 2:
-                        empHrs = 4;
-                        break;
-                    case 1:
-                        empHrs = 8;
-                        break;
-                    default:
-                        empHrs = 0;
-                        break;
-                }
-                totalEmpHrs += empHrs;
-            }
-            totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Total Emp Wage : " + totalEmpWage);
+            Console.WriteLine($"Total Employee Wage is : {EmpWage.computeEmpWage()}");
         }
     }
 }
