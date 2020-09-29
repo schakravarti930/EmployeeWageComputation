4c3634148eadf8c5e43bb635503e6ec28918f5a2
schakravarti <schakravarti930@gmail.com>
2020-09-29 12:10:36 +0530

    [Swapnil] Add : Conditional Wage Calculation

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -8,16 +8,20 @@ namespace EmployeeWageComputation
         public const int IS_PART_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
         public const int NUM_OF_WORKING_DAYS = 20;
+        public const int MAX_HRS_IN_MONTH = 100;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Problem");
             int empHrs = 0;
             int empWage = 0;
             int totalEmpWage = 0;
+            int totalEmpHrs = 0;
+            int totalWorkingDays = 0;
             Random random = new Random();
-            for (int i = 0; i < NUM_OF_WORKING_DAYS; i++)
+            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
             {
                 int empCheck = random.Next(0, 3);
+                totalWorkingDays++;
                 switch (empCheck)
                 {
                     case 2:
@@ -30,9 +34,9 @@ namespace EmployeeWageComputation
                         empHrs = 0;
                         break;
                 }
-                empWage = empHrs * EMP_RATE_PER_HOUR;
-                totalEmpWage += empWage;
+                totalEmpHrs += empHrs;
             }
+            totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Total Emp Wage : " + totalEmpWage);
         }
     }
