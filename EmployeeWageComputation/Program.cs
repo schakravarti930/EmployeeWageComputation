2becaf089fb84094c53503bf0d454619b1ba4d47
schakravarti <schakravarti930@gmail.com>
2020-09-29 11:43:26 +0530

    [Swapnil] Add : Employee Attendance Check

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -6,7 +6,18 @@ namespace EmployeeWageComputation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation Program");
+            Console.WriteLine("Welcome to Employee Wage Computation Problem");
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
         }
     }
 }
